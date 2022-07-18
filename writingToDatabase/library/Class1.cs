e308c31e97c0e62b4b1ea590757105804797580b
KHUTSO SEGODI <77273156+segodiAK@users.noreply.github.com>
2022-07-18 10:50:19 +0200

    Add files via upload

---
+++ writingToDatabase/library/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System.Data.SqlClient;
+
+
+namespace library
+{
+    public class Class1
+    {
+        public static void insertValues(string fName, string lName, string fullName, SqlConnection conn)
+        {
+            
+
+            string query = "INSERT INTO Customer(FirstName,Surname,FullName,isActive)" +
+                " VALUES(@name,@surname,@fullname,1)";
+
+            SqlCommand cmd = new SqlCommand(query,conn);
+           
+
+            cmd.Parameters.AddWithValue("@name", fName);
+            cmd.Parameters.AddWithValue("@surname", lName);
+            cmd.Parameters.AddWithValue("@fullname", fullName);
+
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+            
+        }
+    }
+}
---
+++ writingToDatabase/writingToDatabase/Program.cs
@@ -0,0 +1,28 @@
+﻿using library;
+using System.Data.SqlClient;
+
+namespace writingToDatabase
+{
+    internal class Program
+    {
+       static void Main(string[] args)
+        {
+            string connection =("Server=LAPTOP-LKMUO7D3; Database=mimmosDB; Trusted_Connection=True");
+            SqlConnection conn = new SqlConnection(connection);
+            conn.Open();
+            enterValues(conn);
+         
+        }
+        public static void enterValues(SqlConnection conn)
+        {
+            Console.Write("Please enter your name: ");
+            string Name1 = Console.ReadLine();
+            Console.Write("Please enter your surname: ");
+            string Surname1 = Console.ReadLine();
+            Console.Write("Please enter your full name: ");
+            string FullName1 = Console.ReadLine();
+
+            Class1.insertValues(Name1, Surname1, FullName1, conn);
+        }
+    }
+}
